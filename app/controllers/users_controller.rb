f76aa1ce25ef08343072e18743009817733854dc
github username <github email address>
2020-01-25 16:54:44 -0500

    Added methods new, create, and show to users controller, created a new and show for views/users, create new form to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            #place a flash message here
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def show 
+        @user = User.find_by(id: session[:user_id])
+    end
+
+    private
+
+    def user_params 
+        params.require(:user).permit(:name, :email, :password, :password_conformation)
+    end
+
 end
