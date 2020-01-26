8cc54f09e92261c7ae884ff52749986a6542b414
github username <github email address>
2020-01-26 14:55:09 -0500

    Created methods new, edit, show in repairscontroller with matching repairs views

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -4,5 +4,23 @@ class RepairsController < ApplicationController
         @repairs = Repair.all 
     end
 
+    def new 
+        @repair = Repair.new
+        @rental_properties = RentalProperty.pluck(:property_name, :id)
+    end
+
+    def create
+       if @repair = Repair.create(repair_params)
+            #Flash message here
+            redirect_to repair_path(@repair)
+       else
+            #Flash error here
+            render :new
+       end
+    end
+
+    def show 
+        @repair = Repair.find(params[:id])
+    end
 
 end
