519b5a9358d371ede86d3fce126406aca2712010
github username <github email address>
2020-01-26 17:33:19 -0500

    Added actions index,new,create,show to payments controller, added payment views

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,32 @@
 class PaymentsController < ApplicationController
+    def new 
+        @payment = Payment.new
+        @tenant_ = Tenant.pluck(:renter, :id)
+        @rental_properties = RentalProperty.pluck(:property_name, :id)
+    end
+
+    def create
+       if @payment = Payment.create(payment_params)
+      
+            #Flash message here
+            redirect_to payment_path(@payment)
+       else
+            #Flash error here
+            render :new
+       end
+    end
+
+    def show 
+        @payment = Payment.find(params[:id])
+    end
+
+    
+
+    private
+
+    def payment_params
+        params.require(:payment).permit(:amount_owed, :amount_paid)
+    end
+
+
 end
