da82f00b87ebb5f62faf1289f946cb7ec731041c
github username <github email address>
2020-01-27 15:14:40 -0500

    Added nested routes'

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,26 @@
 Rails.application.routes.draw do
 
   root to: 'sessions#new'
+  
+  get '/logout', to: 'sessions#destroy'
 
   resources :sessions, only: [:new, :create]
   resources :users, only: [:new, :create, :show]
 
   resources :tenants
   resources :repairs
-  resources :rental_properties
+  resources :rental_properties do 
+    resources :tenants 
+  end
+
+  resources :rental_properties do 
+    resources :repairs
+  end
+
+  resources :rental_properties do 
+    resources :payments
+  end
+
   resources :payments
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
