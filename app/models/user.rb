9e37080561d1127607872c41ec352ab5c44cb071
github username <github email address>
2020-01-27 15:11:41 -0500

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
     
     has_secure_password
 
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { in: 4..20 }
+
     has_many :rental_properties
     has_many :tenants, through: :rental_properties
     has_many :repairs, through: :rental_properties
