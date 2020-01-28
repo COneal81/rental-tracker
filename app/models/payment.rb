b43bd056e87b392f0ee640d08833b48d90044ec6
github username <github email address>
2020-01-28 08:16:49 -0500

    Added ActiveRecord validations

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,7 @@
 class Payment < ApplicationRecord
     has_many :rental_properties
     has_many :tenants, through: :rental_properties
+
+    validates :amount_owed, :amount_paid, presence: true
+    end
 end
