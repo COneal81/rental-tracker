99b925ef4698f84afb89f5d5ca2e0d85828651c9
github username <github email address>
2020-01-28 08:18:05 -0500

    Added ActiveRecord validations

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,4 +1,8 @@
 class Tenant < ApplicationRecord
     has_many :rental_properties
     has_many :payments, through: :rental_properties
+
+    validates :renter, :co_renter, :address, :renter_email, :co_renter_email, :renter_cell_phone, 
+        :co_renter_cell_phone, presence: true
+  
 end
