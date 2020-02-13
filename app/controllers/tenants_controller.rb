8d9772fffca3925c85ba087bb29203a2f7a9e393
github username <github email address>
2020-02-13 12:44:27 -0500

    Added before_action set_tenant and created set_tenant private method

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,19 +1,19 @@
 class TenantsController < ApplicationController  
 
-    before_action :logged_in?
+    before_action :logged_in?, :set_tenant, :current_user
 
     def index
-        @tenant = Tenant.all 
+    #    binding.pry
     end
 
     def new 
         @tenant = Tenant.new
-        @rental_property = current_user.rental_properties
     end
 
     def create
-       if @tenant = Tenant.create!(tenant_params)
-      
+         @tenant = current_user.tenants.build(tenant_params)
+         if @tenant.save
+            # binding.pry
             #Flash message here
             redirect_to tenant_path(@tenant)
        else
@@ -23,8 +23,9 @@ class TenantsController < ApplicationController
     end
 
     def show 
+        # binding.pry
         @tenant = Tenant.find(params[:id])
-        @rental_property = current_user.tenants
+          
     end
 
     private
@@ -32,5 +33,9 @@ class TenantsController < ApplicationController
     def tenant_params
         params.require(:tenant).permit(:renter, :co_renter, 
         :address, :renter_email, :co_renter_email, :renter_cell_phone, :co_renter_cell_phone)
+    end
+
+    def set_tenant
+        @tenants = current_user.tenants
     end
 end
