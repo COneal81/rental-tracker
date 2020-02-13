d8b073d5e92b7f56bef43d845c3c03150b8a87f5
github username <github email address>
2020-02-13 12:28:30 -0500

    Removed excess spacing and has many through for payments

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,8 +1,6 @@
 class Tenant < ApplicationRecord
     has_many :rental_properties
-    has_many :payments, through: :rental_properties
-
+    
     validates :renter, :co_renter, :address, :renter_email, :co_renter_email, :renter_cell_phone, 
         :co_renter_cell_phone, presence: true
-  
 end
