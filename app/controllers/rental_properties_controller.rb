d0927e58317e447c362cee1d3ab3ee746dd7b469
github username <github email address>
2020-02-13 12:06:43 -0500

    Refactored methods index, new, create, show, and added redirect to destroy in rental_properties controller

--- app/controllers/rental_properties_controller.rb
+++ app/controllers/rental_properties_controller.rb
@@ -1,22 +1,21 @@
 class RentalPropertiesController < ApplicationController
     before_action :logged_in?, :set_rental_properties
 
-
     def index
-        @rental_properties = current_user.rental_properties    
     end
 
-    def new 
+    def new
         @rental_property = RentalProperty.new
-        @tenant = Tenant.pluck(:renter, :id)
-    
+        @tenant = current_user.tenants
+        #binding.pry
     end
 
     def create
-        if @rental_property = RentalProperty.create!(rental_property_params)
-         
+        @rental_property = current_user.rental_properties.build(rental_property_params)
+        if @rental_property.save
+    
             #Flash message here
-            redirect_to rental_property_path(@rental_property)
+            redirect_to user_rental_property_path(@rental_property.user, @rental_property)
         else 
             #flash message here
             render :new
@@ -26,11 +25,12 @@ class RentalPropertiesController < ApplicationController
     def show 
         
         @rental_property = RentalProperty.find(params[:id])
-        tenant = Tenant.find(params[:id])
-        
+        # binding.pry
     end
 
     def edit
+        @rental_property = RentalProperty.find(params[:id])
+        @tenant = current_user.tenants
     end
 
     def update
@@ -42,13 +42,14 @@ class RentalPropertiesController < ApplicationController
     def destroy
         @rental_property = RentalProperty.find(params[:id])
         @rental_property.destroy
+        redirect_to rental_properties_path
     end
 
     private
 
     def rental_property_params
         params.require(:rental_property).permit(:property_name, :property_description, :address, :monthly_rental_amount,
-        :deposit_amount, :square_feet, :contract_start_date, :contract_end_date, :img_url, :leased)
+        :deposit_amount, :square_feet, :contract_start_date, :contract_end_date, :img_url, :leased, :user_id, :tenant_id)
     end
 
     
