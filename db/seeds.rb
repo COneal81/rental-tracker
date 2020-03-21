5b33efb203b23d4c5fc0336d7ad2efb95c137e2a
github username <github email address>
2020-03-21 12:58:16 -0400

    Updated seed file in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ Repair.destroy_all
 
 
 
+
 #User
 kevin = User.create(name: "Kevin", email: "k@k.com", password: "test123")
 wyatt = User.create(name: "Wyatt", email: "w@w.com", password: "test123")
@@ -22,33 +23,34 @@ wyatt = User.create(name: "Wyatt", email: "w@w.com", password: "test123")
 #Tenant
 kari = Tenant.create(renter: "Kari Smith", co_renter: "N/A", address: "210 Cesna Lane Airport, IA 44444", 
 renter_email: "K@K.com", co_renter_email: "N/A", renter_cell_phone: "123-456-7890", 
-co_renter_cell_phone: "N/A")
+co_renter_cell_phone: "N/A", user_id: wyatt.id)
 
 hudson = Tenant.create(renter: "Hudson Jones", co_renter: "Hunter Jones", address: "2222 One Lane Rd Somewhere, OK 58949", 
 renter_email: "hud@hud.com", co_renter_email: "hunt@hunt.com", renter_cell_phone: "234-567-8901", 
-co_renter_cell_phone: "567-890-1234")
+co_renter_cell_phone: "567-890-1234", user_id: wyatt.id)
 
 griffin = Tenant.create(renter: "Griffin Williams", co_renter: "Massey Williams", address: "4999 Slick Rd Snow, MN 58588", 
 renter_email: "griff@griff.com", co_renter_email: "mass@mass.com", renter_cell_phone: "987-987-9870", 
-co_renter_cell_phone: "987-543-2345")
+co_renter_cell_phone: "987-543-2345", user_id: kevin.id)
 
 cj = Tenant.create(renter: "CJ Pony", co_renter: "n/a", address: "4 Pony Rd Snow, NC 58588", 
     renter_email: "cj@gcj.com", co_renter_email: "n/a", renter_cell_phone: "987-987-9870", 
-    co_renter_cell_phone: "987-543-2345")
+    co_renter_cell_phone: "987-543-2345", user_id: kevin.id)
 
  
+
 #Rental Property
 
 red_ranch = RentalProperty.create(property_name: "Red Ranch", property_description: "Spacious ranch home with a Texas feel!  
 5 Bed/4 Bath!", 
 address: "1234 Main St. Countyline, NC 22222", monthly_rental_amount: 1500, 
 deposit_amount: 1500, square_feet: 2800, leased: true, contract_start_date: "12/1/19", contract_end_date: "12/1/20", user_id: kevin.id, 
-image_url: "https://en.wikipedia.org/wiki/List_of_house_styles#/media/File:HouseII2007.jpg")
+image_url: "https://en.wikipedia.org/wiki/List_of_house_styles#/media/File:HouseII2007.jpg", tenant_id: cj.id)
 
 cape_cod = RentalProperty.create(property_name: "Cape Cod", property_description: "Cute and cozy!  2 Bed ~ 1.5 Bath", 
 address: "123 Liberty Rd Liberty, NC 22221", monthly_rental_amount: 800, 
-deposit_amount: 800, square_feet: 1500, leased: false, contract_start_date: "N/A", contract_end_date: "N/A", user_id: kevin.id, 
-image_url: "https://en.wikipedia.org/wiki/List_of_house_styles#/media/File:Cape_Cod_style_house.jpg", tenant_id: cj.id)
+deposit_amount: 800, square_feet: 1500, leased: true, contract_start_date: "N/A", contract_end_date: "N/A", user_id: kevin.id, 
+image_url: "https://en.wikipedia.org/wiki/List_of_house_styles#/media/File:Cape_Cod_style_house.jpg", tenant_id: griffin.id)
 
 blue_bungalow = RentalProperty.create(property_name: "Blue Bungalow", property_description: "Front porch living at its best!  
 4 Bed ~ 3 
@@ -61,7 +63,6 @@ surraounded by whispering pines.  5 Bed ~ 4.5 Bath  Includes a 10 stall horse ba
 address: "7899 Hidden Valley Rd Farmland, NC 22298", monthly_rental_amount: 2600, 
 deposit_amount: 3000, square_feet: 3500, leased: true, contract_start_date: "7/7/19", contract_end_date: "7/7/20", user_id: wyatt.id, 
 image_url: "https://en.wikipedia.org/wiki/Farm#/media/File:Farming_near_Klingerstown,_Pennsylvania.jpg", tenant_id: kari.id)
-
 
 
 
@@ -79,3 +80,6 @@ repair_completed: false, rental_property_id: blue_bungalow.id)
 
 repair_4 = Repair.create(repair_name: "Window", repair_needed: true, repair_description: "New window on south side of home.  
 Broken by tenant.  Bill tenant", repair_cost: 1000, repair_completed: false, rental_property_id: red_ranch.id)
+
+
+
